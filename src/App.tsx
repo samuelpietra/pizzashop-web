59544a769a1b04b5d286bb824b6202afcfdffac8
Samuel Pietra <samuelpietra@outlook.com>
2024-10-03 19:16:38 -0300

    chore: sets up eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Button } from './components/ui/button';
-import './index.css';
+import './index.css'
+
+import { Button } from '@/components/ui/button'
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <Button>Click me</Button>
     </div>
-  );
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import path from 'node:path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'node:path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
