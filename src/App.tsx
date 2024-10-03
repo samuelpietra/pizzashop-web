d44e7c10738563db96db4aaeb023164e336a9b4e
Samuel Pietra <samuelpietra@outlook.com>
2024-10-03 18:22:34 -0300

    feat: adds Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Button } from './components/ui/button';
 import './index.css';
 
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click me</Button>
+    </div>
+  );
 }
