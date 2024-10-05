9bbb5d04b49c028f03c2244b7b304ac8aafdee2a
Samuel Pietra <samuelpietra@outlook.com>
2024-10-05 11:51:20 -0300

    feat: adds revenue chart

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,63 @@
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  { date: '12/10', revenue: 54 },
+  { date: '12/11', revenue: 115 },
+  { date: '12/12', revenue: 312 },
+  { date: '12/13', revenue: 93 },
+  { date: '12/14', revenue: 58 },
+  { date: '12/15', revenue: 170 },
+  { date: '12/16', revenue: 130 },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Period revenue
+          </CardTitle>
+          <CardDescription>Daily revenue in the period</CardDescription>
+        </div>
+      </CardHeader>
+
+      <CardContent>
+        <ResponsiveContainer height={240} width="100%">
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              width={80}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                })
+              }
+            />
+
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet['500']}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
